f9fe6a8520433414e82fe9f5ac5c816c91cca00c
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2024-05-14 13:46:40 +0200

    Fix toolchain mess

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -1,23 +1,27 @@
 use std::{path::Path, thread};
 
-use eyre::{Context, OptionExt};
+use eyre::{ensure, Context, OptionExt};
 
 fn main() -> eyre::Result<()> {
-    let root_dir = Path::new("..");
+    // Ensure rustup picks up the rust-toolchain.toml file properly and doesn't get confused by this cargo run.
+    std::env::remove_var("CARGO");
+    std::env::remove_var("RUSTUP_TOOLCHAIN");
+
+    let root_dir = Path::new("..")
+        .canonicalize()
+        .wrap_err("canonicalizing ..")?;
     let examples_dir = root_dir.join("code").join("examples");
+
+    // Change the current directory to ensure the correct rustup toolchains are used.
+    std::env::set_current_dir(&examples_dir)
+        .wrap_err("changing current directory to code/examples")?;
 
     let examples = std::fs::read_dir(&examples_dir)
         .wrap_err("opening ../code/examples, script must be run in ./builder")?;
 
-    install_toolchain(&examples_dir).wrap_err("install toolchain")?;
-
+    install_toolchain().wrap_err("install toolchain")?;
     // Setup miri to avoid race condition in `cargo miri run` below...
-    eprintln!("Setting up miri");
-    std::process::Command::new("cargo")
-        .arg("miri")
-        .arg("setup")
-        .output()
-        .wrap_err("setting up miri")?;
+    setup_miri(&examples_dir).wrap_err("setting up miri sysroot")?;
 
     thread::scope(|scope| {
         let mut handles = Vec::new();
@@ -52,6 +56,20 @@ fn main() -> eyre::Result<()> {
     Ok(())
 }
 
+fn setup_miri(dir: &Path) -> eyre::Result<()> {
+    eprintln!("Setting up miri");
+    let output = std::process::Command::new("cargo")
+        .arg("miri")
+        .arg("setup")
+        .current_dir(dir)
+        .spawn()
+        .wrap_err("spawning miri")?
+        .wait()
+        .wrap_err("waiting for miri setup")?;
+    ensure!(output.success());
+    Ok(())
+}
+
 fn run_example(examples_dir: &Path, filename: &str) -> eyre::Result<()> {
     let use_miri = filename.starts_with("unsafe_");
 
@@ -62,14 +80,11 @@ fn run_example(examples_dir: &Path, filename: &str) -> eyre::Result<()> {
     eprintln!("Running {example_name}");
 
     let mut cmd = std::process::Command::new("cargo");
-    cmd.current_dir(examples_dir);
     if use_miri {
         cmd.arg("miri");
     }
     cmd.arg("run").arg("--quiet").arg("--example");
     cmd.arg(example_name);
-
-    remove_rustup_vars(&mut cmd);
 
     let out = cmd.output().wrap_err("spawning cargo")?;
     let stderr = String::from_utf8(out.stderr).wrap_err("stderr was invalid UTF-8")?;
@@ -82,23 +97,15 @@ fn run_example(examples_dir: &Path, filename: &str) -> eyre::Result<()> {
     Ok(())
 }
 
-// Ensure there is output for the toolchain and that the installation doesn't pollute stderr.
-fn install_toolchain(examples_dir: &Path) -> eyre::Result<()> {
+/// Ensures there is output for the toolchain and that the installation doesn't pollute stderr.
+fn install_toolchain() -> eyre::Result<()> {
     let mut toolchain_install = std::process::Command::new("rustc");
     toolchain_install.arg("-V");
-    toolchain_install.current_dir(examples_dir);
-    remove_rustup_vars(&mut toolchain_install);
-    toolchain_install
+    let output = toolchain_install
         .spawn()
         .wrap_err("failed to spawn rustc")?
         .wait()
         .wrap_err("failed to wait for rustc")?;
-
+    ensure!(output.success());
     Ok(())
-}
-
-fn remove_rustup_vars(cmd: &mut std::process::Command) {
-    // Ensure rustup picks up the rust-toolchain.toml file properly and doesn't get confused by this cargo run.
-    cmd.env_remove("CARGO");
-    cmd.env_remove("RUSTUP_TOOLCHAIN");
 }
