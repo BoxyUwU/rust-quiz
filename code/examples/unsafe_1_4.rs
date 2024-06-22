ec09125cc3934e615d224603e171eab4d679f586
Waffle Lapkin <waffle.lapkin@gmail.com>
2024-06-22 23:53:56 +0200

    Add a cursed example to Unsafe 1

--- code/examples/unsafe_1_4.rs
+++ code/examples/unsafe_1_4.rs
@@ -1,6 +1,8 @@
-#![allow(deref_nullptr)]
+#![allow(deref_nullptr, unused_braces)]
 
 fn main() {
-    // 4.  UB?
-    _ = unsafe { *std::ptr::null::<u32>() };
+    // 4. UB?
+    unsafe {
+        _ = { *std::ptr::null::<u32>() };
+    }
 }
