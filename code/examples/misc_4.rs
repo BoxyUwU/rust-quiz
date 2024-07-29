39d8fa4e6fa501d51cb13f07487c22ffea19932b
mrg <miguelraz@ciencias.unam.mx>
2024-07-29 13:05:33 -0600

    add mut hmap to misc_4.rs

--- code/examples/misc_4.rs
+++ code/examples/misc_4.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let map = HashMap::new();
+    let mut map = HashMap::new();
     map.insert(1, 2);
-    assert_eq!(map.get_mut(1), Some(&2));
+    assert_eq!(map.get_mut(&1), Some(&mut 2));
 }
+
