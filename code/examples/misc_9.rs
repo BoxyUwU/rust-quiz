c582b6de147ee732672faff72a0a1902ee785dcb
Jonathan Dönszelmann <jonathan@donsz.nl>
2024-10-10 13:13:51 +0200

    add assignments to misc 9

--- code/examples/misc_9.rs
+++ code/examples/misc_9.rs
@@ -3,17 +3,21 @@ struct Tuple();
 struct Struct {}
 
 fn main() {
-    Unit {};
-    Tuple {};
-    Struct {};
+    Unit;
+    Tuple;
+    Struct;
 
     Unit();
     Tuple();
     Struct();
 
-    Unit;
-    Tuple;
-    Struct;
+    Unit {};
+    Tuple {};
+    Struct {};
+
+    Unit = Unit;
+    Tuple = Tuple();
+    Struct = Struct {};
 
     Unit { .. } = Unit { ..Unit };
     Tuple { .. } = Tuple { ..Tuple() };
