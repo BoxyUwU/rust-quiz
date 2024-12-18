bd5db8bb4b8b1705222024e8e60a803c0e928c29
Alisa Sireneva <me@purplesyringa.moe>
2024-12-18 23:13:59 +0300

    Rework Unsafe 2

--- code/examples/unsafe_2.rs
+++ code/examples/unsafe_2.rs
@@ -1,12 +1,12 @@
 use std::mem::MaybeUninit;
 
 fn main() {
-    // Safety: all bitpatterns are valid for u16
+    // Safety: all bitpatterns are valid for u8
     let random_number: u8 = unsafe { MaybeUninit::uninit().assume_init() };
 
     let very_random_number = if random_number <= 100 {
         unsafe {
-            // Safety: all bitpatterns are valid for u16
+            // Safety: all bitpatterns are valid for u32
             let rng_array: [u32; 100] = MaybeUninit::uninit().assume_init();
             // Safety: The `random_number` is in bounds
             *rng_array.get_unchecked(random_number as usize)
