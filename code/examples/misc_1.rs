9ec99bc50a7126548ebdc630d00dc78a11a0b390
Jana Dönszelmann <jana@donsz.nl>
2025-08-03 10:23:02 +0200

    fixup other stderrs for quiz questions where answers stay as old

--- code/examples/misc_1.rs
+++ code/examples/misc_1.rs
@@ -1,6 +1,9 @@
 pub unsafe const fn my_zeroed<T>() -> T {
     assert!(std::mem::size_of::<T>() <= 256);
-    std::mem::transmute_copy(&[0u8; 256])
+
+    unsafe {
+        std::mem::transmute_copy(&[0u8; 256])
+    }
 }
 
 fn main() {
