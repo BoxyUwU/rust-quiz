e2a875143cf1449935b8828189304eaea1708933
Kivooeo <Kivooeo123@gmail.com>
2025-10-28 21:15:59 +0500

    add misc 7

---
+++ code/examples/misc_7.rs
@@ -0,0 +1,19 @@
+macro_rules! capture {
+    ($e:expr) => {
+        transform!($e)
+    };
+}
+
+macro_rules! transform {
+    ($a:tt + $b:tt) => {
+        $a * $b
+    };
+    ($other:expr) => {
+        $other
+    };
+}
+
+fn main() {
+    let x = capture!(2 + 3);
+    println!("{}", x); // what this will print?
+}
